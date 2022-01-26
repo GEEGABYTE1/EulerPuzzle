8b65eeb2e854ae8d07b0d57bef244f6203a7dfed
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2022-01-26 11:34:41 -0500

    first comparision

--- script.py
+++ script.py
@@ -48,11 +48,52 @@ class Solution:
             row[element] = initial_perm[element]
         break
 
+    
+    def algorithm(self):
+        for term in range(len(self.matrix)):
+            if term == 0:
+                continue 
+            else:
+                for peice_color in range(len(self.lst_setup)):
+                    current_row = self.matrix[term]
+                    prev_row = self.matrix[term - 1] 
+                    current_set_of_peices = self.lst_setup[peice_color]
+
+                    for idx_matrix in range(len(current_row)):
+                        current_peice = list(current_set_of_peices[idx_matrix].keys())[0]
+                        current_colour = current_set_of_peices[idx_matrix][current_peice]
+
+                        upper_peice = list(prev_row[idx_matrix].keys())[0]
+                        upper_colour = list(prev_row[idx_matrix].values())[-1]
+
+                        if upper_colour == current_colour and upper_peice == current_peice:
+                            new_peice = current_set_of_peices.pop()
+                            acc_peice = [new_peice.keys()][0]
+                            new_peice_colour = new_peice[acc_peice]
+
+                            row_information = self.fetch_row_peices(current_row)
+                            current_row_peices = row_information[0]
+                            current_row_colours = row_information[-1]
+                            
+
+                            
+
+                            current_row[idx_matrix] = new_peice
+
+    
+    def fetch_row_peices(self, row):
+        peices = []
+        colours = []
+        for dictionary in row:
+            peices.append(list(dictionary.keys())[0])
+            peices.append(list(dictionary.keys())[-1])
+    
+        return peices, colours
         
-        
+                        
 
 test = Solution()
-print(test)    
+print(test.algorithm())    
     
 
 
