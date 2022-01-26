e65d66083e78c84c42451c79465b98d803323e9d
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2022-01-26 11:04:51 -0500

    Peices Setup

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 ### Current Alg will only run 6 
 from termcolor import colored 
 
+colours = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 
 class Matrix:
     
@@ -8,11 +9,43 @@ class Matrix:
         size_input = str(input("How big of a board would you like to solve (max 6x6): " ))
         size_input = size_input.strip(" ")
         size_input = int(size_input)
+        if size_input > 6:
+            print("That board is currently unavailable")
+            return False
+        else:
+            matrix_grid = [[None for i in range(size_input)] for j in range(size_input)]
 
-        matrix_grid = [[None for i in range(size_input)] for j in range(size_input)]
-
-        return matrix_grid
+            return matrix_grid, size_input
 
 
-test_grid = Matrix()
-print(test_grid.build())
+class Solution:
+    grid_info = Matrix()
+    grid_info = grid_info.build()
+    matrix = grid_info[0]
+    num_peices = grid_info[-1]
+
+    peices = [] 
+    for i in range(num_peices):
+        peices.append(i)
+    
+    colored_peices = []
+    
+    for peice in peices:
+        lst = [] 
+        for colour in colours:
+            dictionary = {peices.pop():colour}
+            lst.append(dictionary)
+        colored_peices.append(lst)
+
+    
+    lst_setup = []
+    for i in range(num_peices):
+        lst_setup.append(colored_peices[0])
+    
+    
+
+test = Solution()
+print(test)    
+    
+
+
